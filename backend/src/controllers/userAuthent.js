628cb75cbdf2d4d6e15dd7e967876091b182ad40
Utkarsh Kumar <utkarshkumar7079@gmail.com>
2025-11-06 18:01:32 +0530

    my first commit

--- backend/src/controllers/userAuthent.js
+++ backend/src/controllers/userAuthent.js
@@ -1,30 +1,50 @@
-const User=require("../models/user");
-const validate=require("../utils/validate");
-const bcrypt= require("bcrypt");
+const User = require("../models/user");
+const validate = require("../utils/validate");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
-const register= async(req,res)=>{
-  try{
-        validate(req.body);
-  const{firstName,emailId,password}=req.body;
-  // check if emailId already exist
-  const existingUser= await User.findOne({emailId});
-  if(existingUser){
-     return res.status(400).json({message:"Email already exists"});
+const register = async (req, res) => {
+  try {
+    validate(req.body);
+    const { firstName, emailId, password } = req.body;
+    // check if emailId already exist
+    const existingUser = await User.findOne({ emailId });
+    if (existingUser) {
+      return res.status(400).json({ message: "Email already exists" });
+    }
+    // create user
+
+    const user = await User.create(req.body);
+    req.body.password = await bcrypt.hash(password, 10);
+
+    // give jwt token to user
+    const token = jwt.sign({ id:user._id,emailId:emailId }, process.env.JWT_SECRET_KEY, {
+      expiresIn: 60 * 60,
+    });
+    res.cookie("token", token, { maxAge: 60 * 60 * 1000 });
+    res.status.send("user registered successfully");
+  } catch (err) {
+    res.status(400).send("Error: " + err);
   }
-  // create user
- 
-  const user= await User.create(req.body);
-  req.body.password= await bcrypt.hash(password,10);
+};
 
-  // give jwt token to user
-  
-
-  }catch(err){
-     res.status(400).send("Error: "+err);
-  }
- 
+const login=async(req,res)=>{
+   try{
+      const{emailId,password}=req.body;
+      if(!emailId)
+         throw new Error("Invalid Credentials")
+      if(!password)
+         throw new Error("Invalid Credentials")
+      const user=await User.findOne({emailId})
+      if(!user)
+         throw new Error("you are not registered . please registered.")
+     const match= bcrypt.compare(password,user.password)
+     if(!match)
+      throw new Error("Invalid Credentials");
+   const token=jwt.sign({id:user._id, emailId:emailId},process.env.JWT_SECRET_KEY,{expiresIn:60*60})
+   res.cookie('token',token,{maxAge:60*60*1000})
+   res.status(200).send("logged in successfully")
+   }catch(err){
+res.status(401).send("Error:"+err)
+   }
 }
-
-
-
-      
