240865341c0f5d3971dcb55e6d8baadbba90b934
Utkarsh Kumar <utkarshkumar7079@gmail.com>
2025-11-13 21:10:40 +0530

    My sixth commit

---
+++ backend/src/routes/problem.js
@@ -0,0 +1,16 @@
+const express= require("express");
+const problemRouter=express.Router();
+const adminMiddleware=require("../middleware/adminMiddleware");
+
+problemRouter.post("/create",adminMiddleware,createProblem);
+problemRouter.patch("/:id",updateProblem);
+problemRouter.delete("/:id",deleteProblem);
+
+// route to fetch a particular problem
+problemRouter.get("/:id",getProblemById);
+// route to fetch all problem
+problemRouter.get("/",getAllProblem);
+// route to fetch all problem solved by user
+problemRouter.get("/user",solvedAllProblembyUser);
+
+module.exports=problemRouter;
